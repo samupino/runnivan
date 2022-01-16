f3498f2a5b4158f49c8be9ecc5ba090ff7a1b807
Samuele <bazlite2@yahoo.it>
2022-01-16 17:11:50 +0100

    improve obstacles speed

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [HideInInspector]
+    public SpeedManager speedManager;
+
     public float deathXDistance;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 newPosition = transform.position + Vector3.left * speedManager.currentSpeed * Time.deltaTime;
+        transform.position = newPosition;
+
         if (transform.position.x < deathXDistance)
         {
             Destroy(this.gameObject);
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -9,22 +9,19 @@ public class ObstacleSpawner : MonoBehaviour
     
     public float spawnRadiusY;
 
-    public GameObject[] obstacles;
+    public Obstacle[] obstacles;
 
     public void SpawnRandom()
     {
         int randomIndex = Random.Range(0, obstacles.Length);
         float yOffset = Random.Range(-spawnRadiusY, spawnRadiusY);
 
-        GameObject obstacle = Instantiate(
+        Obstacle obstacle = Instantiate(
             obstacles[randomIndex],
             transform.position + Vector3.up * yOffset,
             Quaternion.identity
         );
 
-        Rigidbody2D obstacleRB = obstacle.GetComponent<Rigidbody2D>();
-        Vector3 newVelocity = obstacleRB.velocity;
-        newVelocity.x = -speedManager.currentSpeed;
-        obstacleRB.velocity = newVelocity;
+        obstacle.speedManager = speedManager;
     }
 }
