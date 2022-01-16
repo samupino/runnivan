476ef0169e2690fb06993e8e70927a6593eed444
Samuele <bazlite2@yahoo.it>
2022-01-16 18:22:27 +0100

    add score text

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
+    public Text currentScoreText;
+
+    public int pointsPerSecond;
+
+    float _currentScore;
+    public float currentScore
+    {
+        get { return _currentScore; }
+        set
+        {
+            _currentScore = value;
+            currentScoreText.text = currentScore.ToString("n0");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentScore = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentScore += Time.deltaTime * pointsPerSecond;
     }
 }
